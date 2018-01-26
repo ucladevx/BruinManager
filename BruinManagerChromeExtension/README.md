e31c5b83f042b9b8f2a42170427f5cd8f6a58795
rishub <rishub@gmail.com>
2018-01-25 16:24:58 -0800

    added chrome extension

---
+++ BruinManagerChromeExtension/README.md
@@ -0,0 +1,9 @@
+# Bruin Manager Chrome Extension
+
+## Steps to use it
+
+1. Download this folder
+2. Open Google Chrome
+3. Go to chrome://extensions/
+4. Click Load unpacked extension
+5. Select the folder you downloaded
---
+++ BruinManagerChromeExtension/content.js
@@ -0,0 +1,50 @@
+function getEnrollmentAndClassData() {
+	// TODO: check if we are on the study list
+	const enrollmentData = document.getElementById("EnrApptTermTable").textContent // get entire enrollment data
+
+	const enrollmentDataArray = enrollmentData.split("\n") // get each <p> as a separate element
+							.map(string => string.trim()) // remove whitespace
+							.filter(string => true ? string : false) // filter out empty strings
+
+	// TODO: Get class data
+
+	// TODO: parse enrollmentDataArray and class data and store these in chrome storage
+	chrome.storage.sync.set({ 
+		"data": {
+			"enrollmentData": {},
+			"classData": [],
+		}
+	}, () => {
+	    console.log("Stored Class and Enrollment Data")
+	    document.getElementById("loader").style.display = "none";
+	});
+}
+
+
+// if we are on the myucla study list
+if(window.location.href.indexOf("ucla.edu") > -1 && window.location.href.indexOf("studylist") > -1) {
+
+	chrome.storage.sync.get(/* String or Array */["data"], function(data){
+		console.log(data)
+	    if (Object.keys(data).length === 0) { // if we already got their data, don't do it again
+	    	
+	    	// inject loader into html to tell user we are gathering data
+			// CSS is in loader.css
+			const loader = document.createElement("div");
+			loader.id = "loader";
+			// const node = document.createTextNode("Gathering Data...");
+			// loader.appendChild(node);
+			document.body.appendChild(loader);
+	
+	    	getEnrollmentAndClassData()
+		}
+	});
+
+} else if (window.location.href.indexOf("google.com") > -1) { //TODO: change this to domain of bruinmanager when we create a site
+
+	// Get the data that was stored
+	chrome.storage.sync.get(/* String or Array */["data"], function(items){
+	    console.log(items)
+	});
+
+}
