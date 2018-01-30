716e8b80368032e64eae5c8872ffa03547c7e59a
Amir <saad.amir17@gmail.com>
2018-01-30 09:35:21 -0800

    Added user IO

--- BruinManagerChromeExtension/content.js
+++ BruinManagerChromeExtension/content.js
@@ -33,17 +33,19 @@ function getEnrollmentAndClassData() {
 if(window.location.href.indexOf("ucla.edu") > -1 && window.location.href.indexOf("studylist") > -1) {
 
 	chrome.storage.sync.get(/* String or Array */["data"], function(data){
-		// TODO: ask the user if they want us to scrape their data
+		if(confirm("Allow BruinManager to scrape data on this page?")) {
+			// inject loader into html to tell user we are gathering data
+			// CSS is in loader.css
+			const loader = document.createElement("div");
+			loader.id = "loader";
+			// const node = document.createTextNode("Gathering Data...");
+			// loader.appendChild(node);
+			document.body.appendChild(loader);
 
-    	// inject loader into html to tell user we are gathering data
-		// CSS is in loader.css
-		const loader = document.createElement("div");
-		loader.id = "loader";
-		// const node = document.createTextNode("Gathering Data...");
-		// loader.appendChild(node);
-		document.body.appendChild(loader);
-
-    	getEnrollmentAndClassData()
+			getEnrollmentAndClassData()
+		} else {
+			alert("BruinManager stopped!");
+		}
 	});
 
 } else if (window.location.href.indexOf("google.com") > -1) { //TODO: change this to domain of bruinmanager when we create a site
