a9a41ffa8089829e9a0fd3c6be6ae5f834ea64cd
Richard <richbluehill@gmail.com>
2018-02-06 15:32:53 -0800

    added calendar

--- bruinmanager-react/src/MainScreen/SummaryTab/Summary.js
+++ bruinmanager-react/src/MainScreen/SummaryTab/Summary.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  Link 
+  Link
 } from 'react-router-dom'
 import OverviewCalendar from './OverviewCalendar';
 import SuggestedEvents from './SuggestedEvents';
 import Portal from './Portal';
 import { Table } from 'reactstrap';
+import StandardCalendar from './StandardCalendar.js';
+import StandardCalendarStyle from 'react-week-calendar/dist/style.css';
 
 export default class Summary extends React.Component {
 	render() {
 		return (
 			<div style={styles.overall}>
 				<h1>Week {this.props.weekNumber}</h1>
-				<OverviewCalendar/>
+				<StandardCalendar style={StandardCalendarStyle}/>
 				<Table>
 					<td><SuggestedEvents/></td>
 					<td><Portal/></td>
