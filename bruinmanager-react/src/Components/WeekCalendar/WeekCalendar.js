bc973ca2609d5e0c159b48ce076ae8cce31b8441
Taasin <taasin.saquib@gmail.com>
2018-04-29 21:18:47 -0700

    can get user's classes from backend endpoint now

--- bruinmanager-react/src/Components/WeekCalendar/WeekCalendar.js
+++ bruinmanager-react/src/Components/WeekCalendar/WeekCalendar.js
@@ -89,22 +89,18 @@ class WeekCalendar extends React.Component {
     this.calculateColumnDimension();
     window.addEventListener('resize', this.calculateColumnDimension);
     window.addEventListener('mouseup', this.handleSelectionStop);
-    /*
+    
     // get User id with fb info
     var b = localStorage.getItem("myBMData");
     var c = JSON.parse(b);
-    */
-    var url = 'https://arcane-cove-10079.herokuapp.com/api/classes/1857222764329669'
-    /*
-    if(c.id){
-      url = url + c.id;
-    }
-    else{
-      url = url + '1857222764329669';
-    }
-    console.log(url)
+    console.log(c.id);
+    
+    // GET request this url to get user's classes
+    var url = 'https://arcane-cove-10079.herokuapp.com/api/classes/' + c.id;
+    // TODO: get request this url and put classes in the calendar
+    console.log(url);
+
     // end logic to get users data from api
-    */
 
     if(!this.state.isSmall)
         this.refs.calendarbody.refs[0].refs[moment().hours()].refs[moment().hours()].scrollIntoView();
